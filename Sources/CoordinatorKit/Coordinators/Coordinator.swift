611108a489ad89a7510b2fe8ccb468fc1e837ce7
Dal Rupnik <legoless@gmail.com>
2023-02-02 16:27:02 +0100

    Add child coordinator support

--- Sources/CoordinatorKit/Coordinators/Coordinator.swift
+++ Sources/CoordinatorKit/Coordinators/Coordinator.swift
@@ -10,18 +10,27 @@ import Foundation
 import UIKit
 
 /// Coordinator protocol
-/// Each coordinator should own a UIViewController, and have weak reference to parent Coordinator.
+/// Each coordinator should own a UIViewController, and have weak reference to parent Coordinator. Usually the UIViewController would be a UINavigationController
 /// Handle any message in send(message: CoordinatorMessageable) method, unhandled should always be sent to parent.
 /// While handling message make sure the sender is what you expect, because you might get message from another child Coordinator or UIViewController that you are not aware of.
 /// When a child Coordinator is done it should always send a message to it's parent to remove it.
 public protocol Coordinator: Coordinating {
     var parentCoordinator: Coordinator? { get }
     var rootViewController: UIViewController { get }
+    
+    // Optionally implementchild coordinators.
+    func add(child: Coordinator)
 }
 
-extension UIViewController {
+public extension Coordinator {
+    func add(child: Coordinator) {
+        
+    }
+}
+
+public extension UIViewController {
     /// First Coordinator in UIViewController chain, starting with self
-    public var coordinator: Coordinating? {
+    var coordinator: Coordinating? {
         if let coordinator = (self as? CoordinatorInitializable)?.parentCoordinator {
             return coordinator
         } else {
