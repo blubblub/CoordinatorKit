a099999d4be538abb6b5aac2685951a7e59970e0
Dal Rupnik <legoless@gmail.com>
2023-02-02 16:56:24 +0100

    Rework FlowCoordinator

--- Sources/CoordinatorKit/FlowCoordinator/FlowCoordinator.swift
+++ Sources/CoordinatorKit/FlowCoordinator/FlowCoordinator.swift
@@ -9,11 +9,22 @@
 import Foundation
 import UIKit
 
+public enum PresentType {
+    case present
+    case dismiss
+}
+
 /// A Coordinator that knows how to compute flow based on state.
 public protocol FlowCoordinator {
+    func triggerFlow(animated: Bool, type: PresentType)
+}
+
+public extension FlowCoordinator {
+    func presentFlow(animated: Bool) {
+        triggerFlow(animated: animated, type: .present)
+    }
     
-    /// Returns view controller based on FlowState
-    /// - Parameter state: state of flow.
-    /// - Returns: View controller that represents state.
-    func viewController(for state: FlowState) -> UIViewController
+    func dismissFlow(animated: Bool) {
+        triggerFlow(animated: animated, type: .dismiss)
+    }
 }
